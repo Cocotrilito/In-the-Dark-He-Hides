264b45cb105491a47b762640b4aa76afdf85dff4
Cocotrilo <nogalesisaac25@gmail.com>
2026-02-02 23:28:09 -0500

    Final ultra polish game is now 110% DONE

--- control.gd
+++ control.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node2D
 
 func _ready():
 	ScreenFader.fade_in()
@@ -7,6 +7,6 @@ func _ready():
 func _on_play_pressed():
 	await ScreenFader.fade_out()
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
-
+	ScreenFader.fade_in()
 func _on_quit_pressed():
 	get_tree().quit()
--- dead.gd
+++ dead.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Node2D
 
 func _ready():
 	ScreenFader.fade_in()
